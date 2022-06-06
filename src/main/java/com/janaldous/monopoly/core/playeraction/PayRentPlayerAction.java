89a43f6287a8dff7f7d4557862fcfdec22c56fc1
Jat Torres <jat.torres@booking.com>
2022-06-06 20:57:23 +0100

    refactoring

--- src/main/java/com/janaldous/monopoly/core/playeraction/PayRentPlayerAction.java
+++ src/main/java/com/janaldous/monopoly/core/playeraction/PayRentPlayerAction.java
@@ -2,18 +2,14 @@ package com.janaldous.monopoly.core.playeraction;
 
 import com.janaldous.monopoly.core.GameContext;
 import com.janaldous.monopoly.core.Player;
-import com.janaldous.monopoly.core.PlayerImpl;
 import com.janaldous.monopoly.core.exception.NotEnoughMoneyException;
 import com.janaldous.monopoly.core.exception.PlayerActionException;
-import com.janaldous.monopoly.core.gameboard.Gameboard;
 import com.janaldous.monopoly.core.space.PropertySpace;
 import com.janaldous.monopoly.core.space.Space;
-import com.janaldous.monopoly.core.token.Token;
 
 import java.util.Optional;
 
 public class PayRentPlayerAction implements PlayerAction {
-  private static final int MAX_NUMBER_OF_APARTMENTS = 4;
 
   private final GameContext context;
 
@@ -23,26 +19,22 @@ public class PayRentPlayerAction implements PlayerAction {
 
   @Override
   public Optional<PlayerAction> act(Player player) throws PlayerActionException {
-    Token token = context.getPlayerToken(player);
-    Gameboard gameboard = context.getGameboard();
-    Space space = gameboard.getSpace(token);
-    if (space instanceof PropertySpace) {
-      PropertySpace property = (PropertySpace) space;
+    Space space = context.getPlayerSpace(player);
+    if (space instanceof PropertySpace property) {
       int rent = property.getRent();
       Player owner = property.getOwner();
 
-      // owner does not pay rent
+      // owner should not pay rent
       if (owner.equals(player)) {
         return Optional.empty();
       }
 
       if (rent > 0) {
         try {
-          player.pay(rent);
-        } catch (NotEnoughMoneyException neme) {
-          throw new PlayerActionException(neme);
+          context.getBank().transfer(player, owner, rent);
+        } catch (NotEnoughMoneyException e) {
+          throw new PlayerActionException(e);
         }
-        owner.addMoney(rent);
       }
     }
 
