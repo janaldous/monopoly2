b14e83ddde975d6510ad515c4d5bfff8ef7a5a35
Jat Torres <jat.torres@booking.com>
2022-06-11 13:57:53 +0100

    adding comments

--- src/main/java/com/janaldous/monopoly/core/player/sellstrategy/SellPropertyStrategy.java
+++ src/main/java/com/janaldous/monopoly/core/player/sellstrategy/SellPropertyStrategy.java
@@ -5,5 +5,8 @@ import com.janaldous.monopoly.core.space.PropertySpace;
 import java.util.Optional;
 
 public interface SellPropertyStrategy {
+    /**
+     * @return property to sell if player has properties
+     */
     Optional<PropertySpace> chooseProperty();
 }
