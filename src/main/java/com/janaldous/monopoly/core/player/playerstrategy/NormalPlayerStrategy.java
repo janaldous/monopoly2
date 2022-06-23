b58d46c18b9e823798d84e6b3e94b4bd1f59076c
Jat Torres <jat.torres@booking.com>
2022-06-23 20:49:41 +0100

    do not sell property

--- src/main/java/com/janaldous/monopoly/core/player/playerstrategy/NormalPlayerStrategy.java
+++ src/main/java/com/janaldous/monopoly/core/player/playerstrategy/NormalPlayerStrategy.java
@@ -4,6 +4,7 @@ import com.janaldous.monopoly.core.gamecontext.GameContext;
 import com.janaldous.monopoly.core.player.Player;
 import com.janaldous.monopoly.core.playeraction.BuyPropertyPlayerAction;
 import com.janaldous.monopoly.core.playeraction.PlayerAction;
+import com.janaldous.monopoly.core.playeraction.SellPropertyPlayerAction;
 import com.janaldous.monopoly.core.space.PropertySpace;
 
 public class NormalPlayerStrategy implements PlayerStrategy {
@@ -21,6 +22,8 @@ public class NormalPlayerStrategy implements PlayerStrategy {
     if (playerAction instanceof BuyPropertyPlayerAction) {
       if (gameContext.getPlayerSpace(player) instanceof PropertySpace propertySpace)
       return player.getBalance() > propertySpace.getValue();
+    } else if (playerAction instanceof SellPropertyPlayerAction) {
+      return false;
     }
     return true;
   }
